c6c7368e271817ae6f0990038623d66faf5b6863
rid3thespiral <davidebattaglia1993@gmail.com>
2020-08-11 13:24:58 +0200

    first commit, change css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todos : ['fare la spesa', 'fare i compiti', 'chiamare mamma']
+    }
+  }
+  render (){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <h1>
+          TODO LIST 
+        </h1>
+        
+          
       </header>
+      <ul>
+          {
+          this.state.todos.map( (todo,i)=> <li key={i}> {todo} </li> )
+          }
+        </ul>
     </div>
   );
+}
 }
 
 export default App;
