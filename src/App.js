6136070bc5bf98d54405118a1f8cb4c6023dd2aa
rid3thespiral <davidebattaglia1993@gmail.com>
2020-08-12 16:28:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, {Component} from 'react';
+import { createStore } from 'redux';
 import './App.css';
+
+let todos = ['fare', 'dire'];
+
+function storeReducer(state = ['todo1', 'todo2'], action){
+  return state;
+}
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      todos : ['fare la spesa', 'fare i compiti', 'chiamare mamma']
+      todos : [
+         
+      ]
     }
   }
-  render (){
+
+  componentDidMount(){
+    const store = createStore(storeReducer, {todos: [...todos] } );
+    this.setState({todos:[...store.getState().todos]})
+  }
+  
+  render () {
   return (
     <div className="App">
       <header className="App-header">
